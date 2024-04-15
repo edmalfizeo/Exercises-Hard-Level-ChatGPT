a0854488418c37333bbac59c7e9f90051501079f
edmalfizeo <edmalfizeo@gmail.com>
2024-04-15 17:11:40 -0300

    Adding Exercise 4

---
+++ Exercise 4/Exercise 4.py
@@ -0,0 +1,17 @@
+import numpy as np
+matrix = []
+
+row = int(input("Write the size of the row"))
+column = int(input("Write the size of the colums"))
+for i in range(row):
+    a = []
+    for j in range(column):
+        a.append(int(input("Write the numbers that you want do add: ")))
+    matrix.append(a)
+
+array = np.array(matrix)
+matrix_transpose = array.transpose()
+
+print(matrix_transpose)
+   
+            
